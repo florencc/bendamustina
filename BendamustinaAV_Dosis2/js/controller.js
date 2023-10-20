c4c9c5b1ca567c983adbca2437ce366dbf2ad726
florencc <florencia.pereyrav@gmail.com>
2023-10-20 12:45:55 -0300

    update content

---
+++ BendamustinaAV_Dosis2/js/controller.js
@@ -0,0 +1,4 @@
+//Bendamustina
+function navigateToSlide(section){
+	document.location.href = '../BendamustinaAV_'+section+'/BendamustinaAV_'+section+'.html';
+}
---
+++ BendamustinaAV_Riesgos3/js/controller.js
@@ -0,0 +1,4 @@
+//Bendamustina
+function navigateToSlide(section){
+	document.location.href = 'veeva:gotoSlide(BendamustinaAV_'+section+'.zip)';
+}
